1c5e428dfccd146df380f10d2348106c212a68a8
Dietmar Maurer <dietmar@proxmox.com>
2016-04-20 11:21:40 +0200

    add support for ubuntu xenial

--- src/PVE/LXC/Setup/Ubuntu.pm
+++ src/PVE/LXC/Setup/Ubuntu.pm
@@ -12,6 +12,7 @@ use PVE::LXC::Setup::Debian;
 use base qw(PVE::LXC::Setup::Debian);
 
 my $known_versions = {
+    '16.04' => 1, # xenial
     '15.10' => 1, # wily
     '15.04' => 1, # vivid
     '14.04' => 1, # trusty LTS
@@ -47,7 +48,7 @@ sub template_fixup {
 
     my $version = $self->{version};
     
-    if ($version eq '15.04' || $version eq '15.10') {
+    if ($version eq '15.04' || $version eq '15.10' || $version eq '16.04') {
 	# edit /etc/securetty (enable login on console)
 	$self->setup_securetty($conf, qw(pts/0));
     }
