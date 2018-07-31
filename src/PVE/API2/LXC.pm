7c3d228f606a8c784fced194c742543d92d90c46
Dietmar Maurer <dietmar@proxmox.com>
2018-07-31 13:51:55 +0200

    vmlist: document return properties
    Signed-off-by: Dietmar Maurer <dietmar@proxmox.com>

--- src/PVE/API2/LXC.pm
+++ src/PVE/API2/LXC.pm
@@ -74,7 +74,48 @@ __PACKAGE__->register_method({
 	type => 'array',
 	items => {
 	    type => "object",
-	    properties => {},
+	    properties => {
+		vmid => get_standard_option('pve-vmid'),
+		status => {
+		    description => "LXC Container status.",
+		    type => 'string',
+		    enum => ['stopped', 'running'],
+		},
+		maxmem => {
+		    description => "Maximum memory in bytes.",
+		    type => 'integer',
+		    optional => 1,
+		    renderer => 'bytes',
+		},
+		maxswap => {
+		    description => "Maximum SWAP memory in bytes.",
+		    type => 'integer',
+		    optional => 1,
+		    renderer => 'bytes',
+		},
+		maxdisk => {
+		    description => "Root disk size in bytes.",
+		    type => 'integer',
+		    optional => 1,
+		    renderer => 'bytes',
+		},
+		name => {
+		    description => "Container name.",
+		    type => 'string',
+		    optional => 1,
+		},
+		uptime => {
+		    description => "Uptime.",
+		    type => 'integer',
+		    optional => 1,
+		    renderer => 'duration',
+		},
+		cpus => {
+		    description => "Maximum usable CPUs.",
+		    type => 'number',
+		    optional => 1,
+		},
+	    },
 	},
 	links => [ { rel => 'child', href => "{vmid}" } ],
     },
