c564693cf84119573910eadc24fbccc3aeef02f5
Thomas Lamprecht <t.lamprecht@proxmox.com>
2023-06-13 18:43:31 +0200

    setup: cope with non-existing /etc/hosts again
    Fixes: 8f56203 ("fix #4515: template setup: remove bogus LXC_NAME line from /etc/hosts")
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>
    (cherry picked from commit db95dea465e357a63f555a3d3fbd7e02ce7e95ba)
    Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

--- src/PVE/LXC/Setup/Base.pm
+++ src/PVE/LXC/Setup/Base.pm
@@ -844,6 +844,9 @@ sub remove_pve_sections {
 # templates from images.linuxcontainers.org have a bogus LXC_NAME line in /etc/hosts
 sub remove_lxc_name_from_etc_hosts {
     my ($self) = @_;
+
+    return if ! -e '/etc/hosts';
+
     my $hosts = $self->ct_file_get_contents('/etc/hosts');
     my @lines = grep { !/^127.0.1.1\s+LXC_NAME$/ } split(/\n/, $hosts);
 
