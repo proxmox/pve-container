proxmox/pve-container

commit d216e891bea849b60a1dcf3bcb855be75259c4ab

unprivileged: remove bad chown -R call
This was added before we had bind mounts, instead we now
change ownership when creating disks by passing the
`root_owner` option to mkfs or activating+chown()ing the
paths for subvolumes.

src/PVE/LXC.pm
@@ -2213,13 +2213,15 @@ sub get_vm_volumes {
 }
 
 sub mkfs {
-    my ($dev) = @_;
+    my ($dev, $rootuid, $rootgid) = @_;
 
-    PVE::Tools::run_command(['mkfs.ext4', '-O', 'mmp', $dev]);
+    PVE::Tools::run_command(['mkfs.ext4', '-O', 'mmp',
+			     '-E', "root_owner=$rootuid:$rootgid",
+			     $dev]);
 }
 
 sub format_disk {
-    my ($storage_cfg, $volid) = @_;
+    my ($storage_cfg, $volid, $rootuid, $rootgid) = @_;
 
     if ($volid =~ m!^/dev/.+!) {
 	mkfs($volid);
@@ -2240,7 +2242,7 @@ sub format_disk {
     die "cannot format volume '$volid' (format == $format)\n"
 	if $format ne 'raw';
 
-    mkfs($path);
+    mkfs($path, $rootuid, $rootgid);
 }
 
 sub destroy_disks {
@@ -2258,6 +2260,9 @@ sub create_disks {
     my $vollist = [];
 
     eval {
+	my (undef, $rootuid, $rootgid) = PVE::LXC::parse_id_maps($conf);
+	my $chown_vollist = [];
+
 	foreach_mountpoint($settings, sub {
 	    my ($ms, $mountpoint) = @_;
 
@@ -2280,25 +2285,27 @@ sub create_disks {
 		    if ($size_kb > 0) {
 			$volid = PVE::Storage::vdisk_alloc($storecfg, $storage, $vmid, 'raw',
 							   undef, $size_kb);
-			format_disk($storecfg, $volid);
+			format_disk($storecfg, $volid, $rootuid, $rootgid);
 		    } else {
 			$volid = PVE::Storage::vdisk_alloc($storecfg, $storage, $vmid, 'subvol',
 							   undef, 0);
+			push @$chown_vollist, $volid;
 		    }
 		} elsif ($scfg->{type} eq 'zfspool') {
 
 		    $volid = PVE::Storage::vdisk_alloc($storecfg, $storage, $vmid, 'subvol',
 					               undef, $size_kb);
+		    push @$chown_vollist, $volid;
 		} elsif ($scfg->{type} eq 'drbd' || $scfg->{type} eq 'lvm') {
 
 		    $volid = PVE::Storage::vdisk_alloc($storecfg, $storage, $vmid, 'raw', undef, $size_kb);
-		    format_disk($storecfg, $volid);
+		    format_disk($storecfg, $volid, $rootuid, $rootgid);
 
 		} elsif ($scfg->{type} eq 'rbd') {
 
 		    die "krbd option must be enabled on storage type '$scfg->{type}'\n" if !$scfg->{krbd};
 		    $volid = PVE::Storage::vdisk_alloc($storecfg, $storage, $vmid, 'raw', undef, $size_kb);
-		    format_disk($storecfg, $volid);
+		    format_disk($storecfg, $volid, $rootuid, $rootgid);
 		} else {
 		    die "unable to create containers on storage type '$scfg->{type}'\n";
 		}
@@ -2309,6 +2316,13 @@ sub create_disks {
 		# use specified/existing volid
 	    }
 	});
+
+	PVE::Storage::activate_volumes($storecfg, $chown_vollist, undef);
+	foreach my $volid (@$chown_vollist) {
+	    my $path = PVE::Storage::path($storecfg, $volid, undef);
+	    chown($rootuid, $rootgid, $path);
+	}
+	PVE::Storage::deactivate_volumes($storecfg, $chown_vollist, undef);
     };
     # free allocated images on error
     if (my $err = $@) {

src/PVE/LXC/Create.pm
@@ -27,9 +27,6 @@ sub restore_archive {
 
     my ($id_map, $rootuid, $rootgid) = PVE::LXC::parse_id_maps($conf);
     my $userns_cmd = PVE::LXC::userns_command($id_map);
-    if (@$id_map) {
-	PVE::Tools::run_command(['chown', '-R', "$rootuid:$rootgid", $rootdir]);
-    }
 
     my $cmd = [@$userns_cmd, 'tar', 'xpf', $archive, '--totals',
                @$PVE::LXC::COMMON_TAR_FLAGS,

commit 87c458c7d8da83b062a7525afb4131822899f74e

format disk: set FS root uid/gid for passed through /dev/ volumes
When calling mkfs one must pass the root uid/gid parameter along side
as they are used unconditionally, but this wasn't done for the edge
case where a block device from the host was used as volume for the CT,
causing an undef warning.

Note that this code branch is not reachable currently, but that might
change. For now add a FIXME comment to mark this for removal, as we
probably do not want to format devices from /dev/ in any way (and no
user reported that this was broken, so use case seems to be
non-existent).

Fixes: d216e89 ("unprivileged: remove bad chown -R call")
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/LXC.pm
@@ -2048,7 +2048,9 @@ sub format_disk {
     my ($storage_cfg, $volid, $root_uid, $root_gid) = @_;
 
     if ($volid =~ m!^/dev/.+!) {
-	mkfs($volid);
+	# FIXME: remove in Proxmox VE 9 – this code path cannot really be reached currently, using
+	# block devices needs manual preparations by the user
+	mkfs($volid, $root_uid, $root_gid);
 	return;
     }
 