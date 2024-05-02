proxmox/pve-container

commit 02d9462dbe82a434deafa2c957179b484d395278

setup: enable systemd-networkd via preset for ubuntu 23.04+
Signed-off-by: Wolfgang Bumiller <w.bumiller@proxmox.com>

src/PVE/LXC/Setup/Ubuntu.pm
@@ -64,7 +64,7 @@ sub template_fixup {
 
     my $version = $self->{version};
 
-    if ($version >= '17.10') {
+    if ($version >= '17.10' && $version < '23.04') {
 	# enable systemd-networkd
 	$self->ct_mkdir('/etc/systemd/system/multi-user.target.wants');
 	$self->ct_mkdir('/etc/systemd/system/socket.target.wants');
@@ -95,6 +95,10 @@ sub setup_init {
     my ($self, $conf) = @_;
 
     my $version = $self->{version};
+
+    if ($version >= '23.04') {
+	$self->setup_systemd_preset({ 'systemd-networkd.service' => 1 });
+    }
 
     if ($version >= '16.10') {
         $self->setup_container_getty_service($conf);

commit dfcbad017361d4e3ded20af573fbaeacc05231eb

setup: unlink default netplan configuration even with Ubuntu >= 23.04
It seems like commit 02d9462 ("setup: enable systemd-networkd via
preset for ubuntu 23.04+") also resulted in the default netplan
configuration no longer being unlinked. That should still happen, even
if systemd-networkd is now enabled via preset. Otherwise, the network
configuration created by Proxmox VE is not ordered before the one
generated by netplan and thus not applied by systemd-networkd.

Reported in the community forum:
https://forum.proxmox.com/threads/145848/post-658058

Fixes: 02d9462 ("setup: enable systemd-networkd via preset for ubuntu 23.04+")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/LXC/Setup/Ubuntu.pm
@@ -73,7 +73,9 @@ sub template_fixup {
 			  '/etc/systemd/system/multi-user.target.wants/systemd-networkd.service');
 	$self->ct_symlink('/lib/systemd/system/systemd-networkd.socket',
 			  '/etc/systemd/system/socket.target.wants/systemd-networkd.socket');
+    }
 
+    if ($version >= '17.10') {
 	# unlink default netplan lxc config
 	$self->ct_unlink('/etc/netplan/10-lxc.yaml');
     }