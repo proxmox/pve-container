proxmox/pve-container

commit 5582b0c35b450ca1a07515db75893027b7d5aac9

vzdump: rsync: make less verbose
most of that info we get is just plain noise, which adds 15 lines per
sync, so 30 total! Instead just pull out the total transfer info,
i.e., the delta which should be full CT size in the first sync and
the dirty delta in the second.

Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/VZDump/LXC.pm
@@ -23,14 +23,14 @@ my $rsync_vm = sub {
     my ($self, $task, $to, $text, $first) = @_;
 
     my $disks = $task->{disks};
-    my $from = $disks->[0]->{dir} . '/';
+    my $from = $disks->[0]->{dir};
     $self->loginfo ("starting $text sync $from to $to");
 
     my $opts = $self->{vzdump}->{opts};
 
     my @xattr = $task->{no_xattrs} ? () : ('-X', '-A');
 
-    my $rsync = ['rsync', '--stats', @xattr, '--numeric-ids',
+    my $rsync = ['rsync', '--stats', '-h', @xattr, '--numeric-ids',
                  '-aH', '--delete', '--no-whole-file',
                  ($first ? '--sparse' : '--inplace'),
                  '--one-file-system', '--relative'];
@@ -38,7 +38,6 @@ my $rsync_vm = sub {
     push @$rsync, map { "--exclude=$_" } @{$self->{vzdump}->{findexcl}};
     push @$rsync, map { "--exclude=$_" } @{$task->{exclude_dirs}};
 
-    my $starttime = time();
     # See the rsync(1) manpage for --relative in conjunction with /./ in paths.
     # This is the only way to have exclude-dirs work together with the
     # --one-file-system option.
@@ -51,10 +50,18 @@ my $rsync_vm = sub {
     foreach my $disk (@$disks) {
 	push @$rsync, "$from/.$disk->{mp}";
     }
-    $self->cmd([@$rsync, $to]);
+
+    my $transferred = '';
+    my $logfunc = sub {
+	return if $_[0] !~ /^Total transferred file size: (.+)$/;
+	$transferred = $1;
+    };
+
+    my $starttime = time();
+    PVE::Tools::run_command([@$rsync, $to], logfunc => $logfunc);
     my $delay = time () - $starttime;
 
-    $self->loginfo ("$text sync finished ($delay seconds)");
+    $self->loginfo ("$text sync finished - transferred $transferred in ${delay}s");
 };
 
 sub new {

commit b63838a788b6c4ef0a3ff6d0e9fd43938a61d13f

backup: log errors from rsync
Commit 5582b0c ("vzdump: rsync: make less verbose") talks about making
the output less verbose, but it likely did not intended to get rid of
the error messages from rsync, but only the uninteresting messages to
stdout.

The currently used log function is only matching the total
transferred and ignores everything else. Split it into an output and
error function and log all error messages from rsync.

Excerpt from the output with the patch:

> INFO: starting first sync /proc/55667/root/ to /mnt/tmp/vzdumptmp62235_113/
> ERROR: rsync: [sender] send_files failed to open "/proc/55667/root/foo/file": Input/output error (5)
> ERROR: rsync error: some files/attrs were not transferred (see previous errors) (code 23) at main.c(1338) [sender=3.2.7]
> ERROR: Backup of VM 113 failed - command 'rsync --stats -h -X -A --numeric-ids -aH --delete --no-whole-file --sparse --one-file-system --relative '--exclude=/tmp/?*' '--exclude=/var/tmp/?*' '--exclude=/var/run/?*.pid' /proc/55667/root//./ /proc/55667/root//./foo /mnt/tmp/vzdumptmp62235_113/' failed: exit code 23
> INFO: Failed at 2024-05-06 14:21:58
> INFO: Backup job finished with errors

Without the patch, the first two error messages with the root cause of
the issue would not be shown, confusing users and leaving developers
in the dark when trying to help.

Examples from the forum:
https://forum.proxmox.com/threads/146415/post-660946
https://forum.proxmox.com/threads/101810/
https://forum.proxmox.com/threads/101560/
https://forum.proxmox.com/threads/79572/post-352377

Fixes: 5582b0c ("vzdump: rsync: make less verbose")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/VZDump/LXC.pm
@@ -52,13 +52,15 @@ my $rsync_vm = sub {
     }
 
     my $transferred = '';
-    my $logfunc = sub {
+    my $outfunc = sub {
 	return if $_[0] !~ /^Total transferred file size: (.+)$/;
 	$transferred = $1;
     };
 
+    my $errfunc = sub { $self->logerr($_[0]); };
+
     my $starttime = time();
-    PVE::Tools::run_command([@$rsync, $to], logfunc => $logfunc);
+    PVE::Tools::run_command([@$rsync, $to], outfunc => $outfunc, errfunc => $errfunc);
     my $delay = time () - $starttime;
 
     $self->loginfo ("$text sync finished - transferred $transferred in ${delay}s");