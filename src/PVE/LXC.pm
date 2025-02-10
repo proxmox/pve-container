proxmox/pve-container

commit 88ef1700374e6eb11e8be065d0f34bb28e5a0d26

allow nesting to be changed for VM.Allocate on unprivileged containers
instead of it being root only

Signed-off-by: Dominik Csapak <d.csapak@proxmox.com>

src/PVE/LXC.pm
@@ -1270,8 +1270,49 @@ sub check_ct_modify_config_perm {
 		 $opt eq 'searchdomain' || $opt eq 'hostname') {
 	    $rpcenv->check_vm_perm($authuser, $vmid, $pool, ['VM.Config.Network']);
 	} elsif ($opt eq 'features') {
-	    # For now this is restricted to root@pam
-	    raise_perm_exc("changing feature flags is only allowed for root\@pam");
+	    raise_perm_exc("changing feature flags for privileged container is only allowed for root\@pam")
+		if !$unprivileged;
+
+	    my $nesting_changed = 0;
+	    my $other_changed = 0;
+	    if (!$delete) {
+		my $features = PVE::LXC::Config->parse_features($newconf->{$opt});
+		if (defined($oldconf) && $oldconf->{$opt}) {
+		    # existing container with features
+		    my $old_features = PVE::LXC::Config->parse_features($oldconf->{$opt});
+		    for my $feature ((keys %$old_features, keys %$features)) {
+			my $old = $old_features->{$feature} // '';
+			my $new = $features->{$feature} // '';
+			if ($old ne $new) {
+			    if ($feature eq 'nesting') {
+				$nesting_changed = 1;
+				next;
+			    } else {
+				$other_changed = 1;
+				last;
+			    }
+			}
+		    }
+		} else {
+		    # new container or no features defined
+		    if (scalar(keys %$features) == 1 && $features->{nesting}) {
+			$nesting_changed = 1;
+		    } elsif (scalar(keys %$features) > 0) {
+			$other_changed = 1;
+		    }
+		}
+	    } else {
+		my $features = PVE::LXC::Config->parse_features($oldconf->{$opt});
+		if (scalar(keys %$features) == 1 && $features->{nesting}) {
+		    $nesting_changed = 1;
+		} elsif (scalar(keys %$features) > 0) {
+		    $other_changed = 1;
+		}
+	    }
+	    raise_perm_exc("changing feature flags (except nesting) is only allowed for root\@pam")
+		if $other_changed;
+	    $rpcenv->check_vm_perm($authuser, $vmid, $pool, ['VM.Allocate'])
+		if $nesting_changed;
 	} elsif ($opt eq 'hookscript') {
 	    # For now this is restricted to root@pam
 	    raise_perm_exc("changing the hookscript is only allowed for root\@pam");

commit 651478e9fb2c38a61326b863d4d2a73b568981ec

api: config: permission checks: fix check for 'nesting' feature
As reported in the community forum [0], remote migration for a container
with 'features: nesting=0' would fail with a confusing error message

> changing feature flags (except nesting) is only allowed for root@pam

This is because of a faulty check during container creation that would
only detect 'nesting=1', but not 'nesting=0'. Fix it by properly
checking if only the 'nesting' property is affected via an 'exists'
check for the hash key.

The check for deleting the feature is also broken, so fix it too.

The check for modifying the feature already works correctly.

[0]: https://forum.proxmox.com/threads/161432/

Fixes: 88ef170 ("allow nesting to be changed for VM.Allocate on unprivileged containers")
Signed-off-by: Fiona Ebner <f.ebner@proxmox.com>

src/PVE/LXC.pm
@@ -1474,7 +1474,7 @@ sub check_ct_modify_config_perm {
 		    }
 		} else {
 		    # new container or no features defined
-		    if (scalar(keys %$features) == 1 && $features->{nesting}) {
+		    if (scalar(keys %$features) == 1 && exists($features->{nesting})) {
 			$nesting_changed = 1;
 		    } elsif (scalar(keys %$features) > 0) {
 			$other_changed = 1;
@@ -1482,7 +1482,7 @@ sub check_ct_modify_config_perm {
 		}
 	    } else {
 		my $features = PVE::LXC::Config->parse_features($oldconf->{$opt});
-		if (scalar(keys %$features) == 1 && $features->{nesting}) {
+		if (scalar(keys %$features) == 1 && exists($features->{nesting})) {
 		    $nesting_changed = 1;
 		} elsif (scalar(keys %$features) > 0) {
 		    $other_changed = 1;