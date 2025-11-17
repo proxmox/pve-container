proxmox/pve-container

commit 2aed26d320aebfed89d98c85e2009b36289d10d6

add support for OCI images as container templates
This aims to add basic support for the Open Container Initiative image
format according to the specification. [0]

[0] https://github.com/opencontainers/image-spec/blob/main/spec.md

Signed-off-by: Filip Schauer <f.schauer@proxmox.com>
Link: https://lore.proxmox.com/20251008171028.196998-7-f.schauer@proxmox.com
Signed-off-by: Thomas Lamprecht <t.lamprecht@proxmox.com>

src/PVE/API2/LXC.pm
@@ -19,9 +19,11 @@ use PVE::Storage;
 use PVE::RESTHandler;
 use PVE::RPCEnvironment;
 use PVE::ReplicationConfig;
+use PVE::RS::OCI;
 use PVE::LXC;
 use PVE::LXC::Create;
 use PVE::LXC::Migrate;
+use PVE::LXC::Namespaces;
 use PVE::GuestHelpers;
 use PVE::VZDump::Plugin;
 use PVE::API2::LXC::Config;
@@ -536,19 +538,91 @@ __PACKAGE__->register_method({
 
                 eval {
                     my $rootdir = PVE::LXC::mount_all($vmid, $storage_cfg, $conf, 1);
+                    my $archivepath = '-';
+                    $archivepath = PVE::Storage::abs_filesystem_path($storage_cfg, $archive)
+                        if ($archive ne '-');
                     $bwlimit = PVE::Storage::get_bandwidth_limit(
                         'restore', [keys %used_storages], $bwlimit,
                     );
-                    print "restoring '$archive' now..\n"
-                        if $restore && $archive ne '-';
-                    PVE::LXC::Create::restore_archive(
-                        $storage_cfg,
-                        $archive,
-                        $rootdir,
-                        $conf,
-                        $ignore_unpack_errors,
-                        $bwlimit,
-                    );
+                    my $is_oci = 0;
+
+                    if ($restore && $archive ne '-') {
+                        print "restoring '$archive' now..\n";
+                    } elsif ($archivepath =~ /\.tar$/) {
+                        # Check whether archive is an OCI image
+                        my ($has_oci_layout, $has_index_json, $has_blobs) = (0, 0, 0);
+                        PVE::Tools::run_command(
+                            ['tar', '-tf', $archivepath],
+                            outfunc => sub {
+                                my $line = shift;
+                                $has_oci_layout = 1 if $line eq 'oci-layout';
+                                $has_index_json = 1 if $line eq 'index.json';
+                                $has_blobs = 1 if $line =~ /^blobs\//m;
+                            },
+                        );
+
+                        $is_oci = 1 if $has_oci_layout && $has_index_json && $has_blobs;
+                    }
+
+                    if ($is_oci) {
+                        # Extract the OCI image
+                        my ($id_map, undef, undef) = PVE::LXC::parse_id_maps($conf);
+                        my $oci_config = PVE::LXC::Namespaces::run_in_userns(
+                            sub {
+                                PVE::RS::OCI::parse_and_extract_image($archivepath, $rootdir);
+                            },
+                            $id_map,
+                        );
+
+                        # Set the entrypoint and arguments if specified by the OCI image
+                        my @init_cmd = ();
+                        push(@init_cmd, @{ $oci_config->{Entrypoint} })
+                            if $oci_config->{Entrypoint};
+                        push(@init_cmd, @{ $oci_config->{Cmd} }) if $oci_config->{Cmd};
+                        if (@init_cmd) {
+                            my $init_cmd_str = shift(@init_cmd);
+                            if (@init_cmd) {
+                                $init_cmd_str .= ' ';
+                                $init_cmd_str .= join(
+                                    ' ',
+                                    map {
+                                        my $s = $_;
+                                        $s =~ s/"/\\"/g;
+                                        qq{"$_"}
+                                    } @init_cmd,
+                                );
+                            }
+                            if ($init_cmd_str ne '/sbin/init') {
+                                push @{ $conf->{lxc} }, ['lxc.init.cmd', $init_cmd_str];
+
+                                # An entrypoint other than /sbin/init breaks the tty console mode.
+                                # This is fixed by setting cmode: console
+                                $conf->{cmode} = 'console';
+                            }
+                        }
+
+                        push @{ $conf->{lxc} }, ['lxc.init.cwd', $oci_config->{WorkingDir}]
+                            if ($oci_config->{WorkingDir});
+
+                        if (my $envs = $oci_config->{Env}) {
+                            for my $env (@{$envs}) {
+                                push @{ $conf->{lxc} }, ['lxc.environment.runtime', $env];
+                            }
+                        }
+
+                        my $stop_signal = $oci_config->{StopSignal} // "SIGTERM";
+                        push @{ $conf->{lxc} }, ['lxc.signal.halt', $stop_signal];
+                    } else {
+                        # Not an OCI image, so restore it as an LXC image instead
+                        PVE::LXC::Create::restore_archive(
+                            $storage_cfg,
+                            $archive,
+                            $rootdir,
+                            $conf,
+                            $ignore_unpack_errors,
+                            $bwlimit,
+                        );
+                    }
 
                     if ($restore) {
                         print "merging backed-up and given configuration..\n";

commit a33cefb1b98311f2fcdff3d9b349c73166990559

fix regression breaking container restore from PBS
This fixes a regression caused by an abs_filesystem_path call introduced
with OCI image support. When trying to restore a container from a
Proxmox Backup Server, this tried to resolve a path on a PBS datastore
as a local file system path. This is fixed by only calling
abs_filesystem_path on path-based storages.

Fixes: 2aed26d320ae ("add support for OCI images as container
templates")

Signed-off-by: Filip Schauer <f.schauer@proxmox.com>
Reviewed-by: Fabian Grünbichler <f.gruenbichler@proxmox.com>
Link: https://lore.proxmox.com/20251117135205.157724-1-f.schauer@proxmox.com

src/PVE/API2/LXC.pm
@@ -543,8 +543,12 @@ __PACKAGE__->register_method({
                 eval {
                     my $rootdir = PVE::LXC::mount_all($vmid, $storage_cfg, $conf, 1);
                     my $archivepath = '-';
-                    $archivepath = PVE::Storage::abs_filesystem_path($storage_cfg, $archive)
-                        if ($archive ne '-');
+                    if ($archive ne '-') {
+                        my $archive_sid = (PVE::Storage::parse_volume_id($archive))[0];
+                        my $scfg = PVE::Storage::storage_config($storage_cfg, $archive_sid);
+                        $archivepath = PVE::Storage::abs_filesystem_path($storage_cfg, $archive)
+                            if $scfg->{path};
+                    }
                     $bwlimit = PVE::Storage::get_bandwidth_limit(
                         'restore', [keys %used_storages], $bwlimit,
                     );